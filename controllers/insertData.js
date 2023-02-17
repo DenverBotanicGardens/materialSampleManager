fde2c087d947d0a7cd79b74c065c0de57c286d36
Rick Levy <ricklevy21@gmail.com>
2023-02-17 11:45:24 -0700

    inserting records into occurrence, materialSample, and preservedSpecimen. Need to handle dates, and issue with storageLocation

--- controllers/insertData.js
+++ controllers/insertData.js
@@ -1,37 +1,21 @@
 const db = require("../models");
 const MaterialSample = db.materialSample;
 const Occurrence = db.occurrence;
+const PreservedSpecimen = db.preservedSpecimen;
 
 async function insertData(req,res) {
-  //create some data
-  // const data = [
-  //   {
-  //     recordedBy: "Rick Levy",
-  //     materialSamples:
-  //       {
-  //         materialSampleType: "tissue",
-  //         numberCollected: 2
-  //       }
-  //   },
-  //   {
-  //     recordedBy: "Shawn Cohen",
-  //     materialSamples:
-  //     {
-  //       materialSampleType: "seed",
-  //       numberCollected: 72
-  //     }
-  //   }
-  // ]
   //use bulkCreate with include to insert the data
   const result = await Occurrence.bulkCreate(req.body, {
     include : [
       {
         model: MaterialSample
+      },
+      {
+        model: PreservedSpecimen
       }
     ]
   })
   .then(() => {
-    //console.log(data)
     res.send()
   })
   .catch((err) => {
