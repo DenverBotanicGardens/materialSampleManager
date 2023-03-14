f53ff63e7a660d1ec12c5542ca67182a71e17c31
Rick Levy <ricklevy21@gmail.com>
2023-03-14 12:38:44 -0600

    route and controller set up to add new germination test

---
+++ controllers/addGerminationTest.js
@@ -0,0 +1,35 @@
+const db = require("../models");
+const GermplasmViabilityTest = db.germplasmViabilityTest 
+const Sequelize = require("sequelize");
+const { sequelize } = require("../models");
+const { QueryTypes } = require('sequelize');
+const Op = Sequelize.Op;
+
+async function addGerminationTest(req,res) {
+    //use bulkCreate with include to insert the data
+    const result = await GermplasmViabilityTest.create({
+        materialSample_catalogNumber : req.body.materialSample_catalogNumber,
+        testConductedBy : req.body.testConductedBy,
+        sampleFrozen : req.body.sampleFrozen,
+        medium: req.body.medium,
+        scarified: req.body.scarified,
+        stratificationTemperature: req.body.stratificationTemperature,
+        stratificationStartDate : req.body.stratificationStartDate,
+        incubationStartDate : req.body.incubationStartDate,
+        numberSeedsTested : req.body.numberSeedsTested,
+        incubationTempDay : req.body.incubationTempDay,
+        incubationTempNight : req.body.incubationTempNight,
+        materialSampleTableID : req.body.materialSampleTableID
+    })
+    .then((data) => {
+      res.send(data)
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  
+  }
+  
+  module.exports = {
+    addGerminationTest
+}
