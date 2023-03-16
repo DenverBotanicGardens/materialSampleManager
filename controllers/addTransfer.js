878166592fef39be313d2a7010feb2f17a530752
Rick Levy <Richard.Levy@BotanicGardens.org>
2023-03-16 14:36:00 -0600

    route for adding transfers working

--- controllers/addTransfer.js
+++ controllers/addTransfer.js
@@ -7,23 +7,49 @@ const Op = Sequelize.Op;
 
 
 async function addTransfer(req,res) {
-    console.log(req.body)
-    var insert = []
-
+    var insertInto = []
+    var insert = {}
     for (let i = 0; i < req.body.length; i++){
-        for(let k in req.body){
-            if( req.body[k] === '' )
-                delete req.body[k]
-    //   const result = await Transfer.bulkCreate(insert)
-    //   .then((data) => {
-    //     res.send(data)
-    //   })
-    //   .catch((err) => {
-    //     console.log(err)
-    //   })
+        if (req.body[i].materialSample_catalogNumber){
+            insert.materialSample_catalogNumber = req.body[i].materialSample_catalogNumber
+            }
+        if (req.body[i].numberSamplesTransferred){
+            insert.numberSamplesTransferred = req.body[i].numberSamplesTransferred
+            }
+        if (req.body[i].transferDate){
+            insert.transferDate = req.body[i].transferDate
+            }
+        if (req.body[i].receivedDate){
+            insert.receivedDate = req.body[i].receivedDate
+            }
+        if (req.body[i].agencyTransferredTo){
+            insert.agencyTransferredTo = req.body[i].agencyTransferredTo
+            }
+        if (req.body[i].personTransferredTo){
+            insert.personTransferredTo = req.body[i].personTransferredTo
+            }
+        if (req.body[i].purposeNotes){
+            insert.purposeNotes = req.body[i].purposeNotes
+            }
+        if (req.body[i].returnedDate){
+            insert.returnedDate = req.body[i].returnedDate
+            }
+        if (req.body[i].numberSamplesReturned){
+            insert.numberSamplesReturned = req.body[i].numberSamplesReturned
+            }
+        if (req.body[i].materialSampleTableID){
+            insert.materialSampleTableID = req.body[i].materialSampleTableID
+            }
+        insertInto.push(insert)
+    }
+      const result = await Transfer.bulkCreate(insertInto)
+      .then((data) => {
+        res.send(data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
 }
-console.log(req.body)
-}}
 
 module.exports = {
     addTransfer
