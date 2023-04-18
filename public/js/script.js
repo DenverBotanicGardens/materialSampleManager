3c0302ce8a0dbfc33f8ea574be84fdddfdc8f989
Rick Levy <Richard.Levy@BotanicGardens.org>
2023-04-18 14:52:48 -0600

    setting up script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,42 @@
 $(document).ready(function() {
     console.log("hi")
+//--------------------------------------------------------------------------------------------------
+    //GLOBAL VARIABLES
+//--------------------------------------------------------------------------------------------------
+    //Search Form
+    let searchFormEntires = {}
+
+    //New Project Form
+    let newProjectFormEntries = {}
+
+    //Search Germination Trials Form
+    let searchGermTrialsFormEntries = {}
+
+    //Search for Seed to add to Germinamtion Trial Form
+    let searchSeedForGermTrialFormEntries = {}
+
+    //New Germination Trial Form
+    let newGermTrialFormEntries = {}
+
+    //Add Viability Tracking Form
+    let addViabilityTrackingFormEntries = {}
+
+    //Finish Germination Trial Form
+    let finsihGermTrialFormEntries = {}
+
+    //Search For Transfers Form
+    let searchTransfersFormEntries = {}
+
+    //Search for Material Samples to Transfer Form
+    let searchSamplesToTransferFormEntries = {}
+
+    //New Transfer Form
+//--------------------------------------------------------------------------------------------------
+    // EVENT LISTENERS
+//--------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------
+    //FUNCTIONS
+//--------------------------------------------------------------------------------------------------
+
+
 })
