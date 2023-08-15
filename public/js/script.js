aeead48d04616db97ab34d29a7adca72845f3122
Rick Levy <ricklevy21@gmail.com>
2023-08-15 09:54:36 -0600

    added coments and array variables to the script page

--- public/js/script.js
+++ public/js/script.js
@@ -4,48 +4,87 @@ $(document).ready(function() {
     //GLOBAL VARIABLES
     //--------------------------------------------------------------------------------------------------
     //Search Form
+    //an object to contain the values entered by the user into the form on the Search page
     let searchFormEntires = {}
     
     //New Project Form
+    //an object to contain the values entered by the user into the form to create a new project
     let newProjectFormEntries = {}
     
     //Projects List
-    let projectsLit = []
+    //an array that contains all projects from the database. to be displayed on the list on the projects page and in the dropdown selection on the upload data page.
+    let projectsList = []
 
     //Project Selection
+    //an array to contain the project selected on the upload data page, for the data uploaded to be a part of.
     let projectSelection = []
     
     //Search Germination Trials Form
+    //an object to contain the values entered by the user in the search for germination trials form on the germination trials page.
     let searchGermTrialsFormEntries = {}
 
+    //Germination Trials Search Results
+    //an array of objects to contain the list of germination trials retured to the user based upon their query
+    let germinationTrialsSearchResults = []
+
     //Search for Seed to add to Germinamtion Trial Form
+    //an object to contain the values entered by the user in the search for seed to add to trial form on the create new germination trial page.
     let searchSeedForGermTrialFormEntries = {}
 
+    //Seed for Germination Trial List
+    //an array to contain the list of seeds provided after a search is conducted on the create new germination trial page.
+    let seedForGerminationTrial = []
+
     //New Germination Trial Form
+    //an object to contain the values entered by the user in the new trial form on the the create new germintaion trial page.
     let newGermTrialFormEntries = {}
 
     //Add Viability Tracking Form
+    //an object to contain the values entered by the user in the add viability tracking data form on the add viability tracking page.
     let addViabilityTrackingFormEntries = {}
 
+    //Germintaion Trial Metadata while Tracking Viability
+    //an object to contain the metadata describing the Germination Trial so that it can be viewed and referenced byt he user while adding viability tracking
+    let germinationTrialMetadataTracking = {}
+
     //Finish Germination Trial Form
+    //an object to contain the values entered by the user in the finish germination trial form on the finish germiantion trial page.
     let finsihGermTrialFormEntries = {}
 
     //Search For Transfers Form
+    //an object to contain the values entered by the user in the search for transfers form on the transfers page
     let searchTransfersFormEntries = {}
 
+    //Transfers Result List
+    //an array of objects to contain the transfer records returned based on the query submitted by the user
+    let transferSearchResults = []
+
     //Search for Material Samples to Transfer Form
+    //an object to contain the values entered byt he user in the seach for material sample to transfer form on the transfer material sample page
     let searchSamplesToTransferFormEntries = {}
 
+    //Material Samples for Transfer
+    //an array of objects to contain the results from the search for material samples to transfer ont he transfer material sample page
+    let materialSamplesToTransferSearchResults = []
+
     //New Transfer Form
+    //an object to contain the values entered by the user in the record transfer information here form on the transfer material sample page
     let newTransferFormEntries = {}
 
     //Update Transfer Form
+    //an object to contain the values entered by the user in the update material sample transfer form on the update transfer page
     let updatTransferFormEntries = {}
 
     //Search for Material Sample to Update Form
+    //an object to contain the values entered by the user in the search for material samples to update form on the update material sample page
     let searchSamplesToUpdateFormEntries = {}
 
+    //Material Samples to Update
+    //an array of objects to contain the material sample records returned based on the query submitted by the user on the update material sample page
+    let materialSamplesToUpdateSearchResults = []
+
     //Update Material Sample Form
+    //an object to contian the values entered by the user in the sample updates form on the update material sample page
     let updateMaterialSampleFormEntries = {}
 
 //--------------------------------------------------------------------------------------------------
