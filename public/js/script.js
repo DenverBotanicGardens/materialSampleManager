82e0ab3067eab50d4ed16433fcdf7fe2ef5184bb
Rick Levy <ricklevy21@gmail.com>
2023-08-22 14:27:35 -0600

    user can now select a germination trial to finish and submit the final data points to the db

--- public/js/script.js
+++ public/js/script.js
@@ -46,75 +46,79 @@ $(document).ready(function() {
     //an object to contain the metadata describing the Germination Trial so that it can be viewed and referenced byt he user while adding viability tracking
     let germinationTrialMetadataTracking = {}
 
-    //Finish Germination Trial Form
-    //an object to contain the values entered by the user in the finish germination trial form on the finish germiantion trial page.
-    let finsihGermTrialFormEntries = {}
-
+    
     //Search For Transfers Form
     //an object to contain the values entered by the user in the search for transfers form on the transfers page
     let searchTransfersFormEntries = {}
-
+    
     //Transfers Result List
     //an array of objects to contain the transfer records returned based on the query submitted by the user
     let transferSearchResults = []
-
+    
     //Search for Material Samples to Transfer Form
     //an object to contain the values entered byt he user in the seach for material sample to transfer form on the transfer material sample page
     let searchSamplesToTransferFormEntries = {}
-
+    
     //Material Samples for Transfer
     //an array of objects to contain the results from the search for material samples to transfer ont he transfer material sample page
     let materialSamplesToTransferSearchResults = []
-
+    
     //New Transfer Form
     //an object to contain the values entered by the user in the record transfer information here form on the transfer material sample page
     let newTransferFormEntries = {}
-
+    
     //Update Transfer Form
     //an object to contain the values entered by the user in the update material sample transfer form on the update transfer page
     let updatTransferFormEntries = {}
-
+    
     //Search for Material Sample to Update Form
     //an object to contain the values entered by the user in the search for material samples to update form on the update material sample page
     let searchSamplesToUpdateFormEntries = {}
-
+    
     //Material Samples to Update
     //an array of objects to contain the material sample records returned based on the query submitted by the user on the update material sample page
     let materialSamplesToUpdateSearchResults = []
-
+    
     //Update Material Sample Form
     //an object to contian the values entered by the user in the sample updates form on the update material sample page
     let updateMaterialSampleFormEntries = {}
-
+    
     //Seed sample selected for Germination Trial
     //variables to hold the id and catalogNumber for the seed sample selected for a new germination trial
     let seedSampleSelectedID
     let seedSampleSelectedCatalogNumber
-
+    
     //Filename for CSV containing Germination Trial Results Data that is to be downloaded by the client
     let germinationTrialResultsCSVFileName
-
+    
     //Germination Trial Sellected for Add Viability Tracking
     let germinationTrialIDSelectedAddViability
     let getOneGermTrialQuery = {}
     let germinationTrialIDForAddingViabilityTracking
     let newAddViabilityTrackingFormEntries ={}
+    
+    //Germination Trial Selected for Finishing Trial
+    let germinationTrialIDSelectedFinish
+    let germinationTrialIDForFinishingTrial
+    let newFinishTrialFormEntries ={}
 
-//--------------------------------------------------------------------------------------------------
-// EVENT LISTENERS
-//--------------------------------------------------------------------------------------------------
-//Hide Elements
+
+    //--------------------------------------------------------------------------------------------------
+    // EVENT LISTENERS
+    //--------------------------------------------------------------------------------------------------
+    //Hide Elements
     //Search Page
     $('#searchResults').hide()
-
+    
     //Germination Trials Page
     $('#searchGermTrialResults').hide()
     $("#addViabilityTrackingFormAndMetadata").hide()
+    $("#finishGerminationTrialFormAndMetadata").hide()
     
     //Create New Germination Trial Page
     $('#seedSearchGermTrialResults').hide()
     $('#newGermTrialDataForm').hide()
-
+    
     //Transfers Page
     $('#searchTransfersResults').hide()
 
@@ -475,12 +479,7 @@ $(document).ready(function() {
         submitViabilityTracking()
     })
     
-    //function to send to backend via api
-    // const submitViabilityTracking = (viabilityTracking) => {
-    //     $.post("api/addViabilityTracking", viabilityTracking, function(){
-    //         window.location.reload();
-    //     })
-    // } 
+    //function to send Add Viability Tracking Form Inputs to backend via api
     const submitViabilityTracking = () => {
         $.ajax({
             method: "POST",
@@ -496,10 +495,83 @@ $(document).ready(function() {
     }
 
 
-//Finish GerminatioN Trial
+//Finish Germination Trial
     //Event listener for button to capture ID of selected germination trial
-    //retrieve user entries and add to finsihGermTrialFormEntries object
-    //send to backend via api
+    $(document).on('click','button[data-finishTrialButton="true"]',function(event){
+        //event.preventDefault()
+        germinationTrialIDSelectedFinish = $(this).data('id')
+        getOneGermTrialQuery = {
+            id: germinationTrialIDSelectedFinish
+        }
+        $("#searchGermTrialsFormPage").hide()
+        $("#searchGermTrialResults").hide()
+        $("#createNewGerminationTrialHref").hide()
+        $("#finishGerminationTrialFormAndMetadata").show()
+        getGermTrialMetadataForFinish()
+    })
+    
+    //function to get germiantion trial metadata and display on add viability tracking page
+    getGermTrialMetadataForFinish = () => {
+        $.ajax({
+            method: "POST",
+            url: "/api/getGermplasmViabilityTestByID",
+            data: getOneGermTrialQuery
+        })
+        .then((trialFromDB) => {
+            germinationTrialIDForFinishingTrial = trialFromDB[0].id
+            let seedScarified
+            let frozen
+            if (trialFromDB[0].scarified === 1){
+                seedScarified = "Yes"
+            } else {
+                seedScarified = "No"
+            }
+            if (trialFromDB[0].sampleFrozen === 1){
+                frozen = "Yes"
+            } else {
+                frozen = "No"
+            }
+            $("#finishGerminationTrialMetadata1").append(`${trialFromDB[0].scientificName} seed (${trialFromDB[0].materialSample_catalogNumber}) collected from ${trialFromDB[0].locationID} in ${trialFromDB[0].county} County on ${trialFromDB[0].eventDate} by ${trialFromDB[0].recordedBy}.`)
+            $("#finishGerminationTrialMetadata2").append(`Pretreatments: | Seed Scarified: ${seedScarified} | Sample Frozen: ${frozen} | Stratification Start Date: ${trialFromDB[0].stratificationStartDate} | Stratification Temperature: ${trialFromDB[0].stratificationTemperature} |  Incubation Start Date ${trialFromDB[0].incubationStartDate} | Incubation Temp Day: ${trialFromDB[0].incubationTempDay} | Incubation Temp Night: ${trialFromDB[0].incubationTempNight} | Trial Started With ${trialFromDB[0].numberSeedsTested} Seeds.`)
+        })
+    }
+    
+    //retrieve user entries and add to addViabilityTrackingFormEntries object
+    let endDateInput = $("#finishTrialEndDate")
+    let numberDeadInput = $("#finishTrialNumberDead")
+    let numberViableInput = $("#finishTrialNumberViable")
+    let totalGerminantsInput = $("#finishTrialTotalGerminants")
+    let finishGerminationTrialForm = $("#finishGerminationTrialForm")
+    $(finishGerminationTrialForm).on("submit", function handleFormSubmit(event){
+        event.preventDefault()
+        if (!endDateInput.val().trim() || !numberDeadInput.val().trim() || !numberViableInput.val().trim() || !totalGerminantsInput.val().trim()){
+            alert("Please provide a valid value for all fields!")
+        }
+        newFinishTrialFormEntries = {
+            endDate: endDateInput.val().trim(),
+            numberDead: numberDeadInput.val().trim(),
+            numberViable: numberViableInput.val().trim(),
+            totalGerminants: totalGerminantsInput.val().trim(),
+            id: germinationTrialIDForFinishingTrial
+        }
+        submitFinishTrial()
+    })
+    
+    //function to send Finish Germination Trial form inputs to backend via api
+    const submitFinishTrial = () => {
+        $.ajax({
+            method: "PUT",
+            url: "/api/finishGerminationTest",
+            data: newFinishTrialFormEntries,
+            success: function(){
+                alert('Success! You have finished this germination trial. Keep up the gret work!')
+            }
+        })
+        .then(function(){
+            window.location.href = '/germinationTrials'
+        })
+    }
+
 
 
 //Search for Seed for Germination Trial
