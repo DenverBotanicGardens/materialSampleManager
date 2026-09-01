5bb8a7c56fa09431706a712b9438c2aa821ff201
Rick Levy <ricklevy21@gmail.com>
2026-09-01 16:02:59 -0600

    sql security patch searchCollections

--- controllers/searchCollections.js
+++ controllers/searchCollections.js
@@ -4,14 +4,13 @@ const { sequelize } = require("../models");
 const { QueryTypes } = require('sequelize');
 const Op = Sequelize.Op;
 const fs = require("fs")
+const path = require("path")
 const csv = require("fast-csv")
 
 //variable to hold the data so that it can be exported after a search
 var dataForExport
 
-//empty array to populate with parameters that make up where clause
-var materialSampleCollectionsQuery = []
-//standard query to return all records from transfers table. extra where params provided by user are concatted to the end of this string
+//standard query to return all records from transfers table. extra where params provided by user are added via named replacements
 var materialSampleCollecitonsSelect = `SELECT p.id AS projectTableID, p.project, p.principalInvestigator, p.dbgContact, p.PIemail, o.id AS occurrenceTableID, o.occurrenceID, o.recordedBy, o.eventDate, o.scientificName, o.identifiedBy, o.dateIdentified, o.associatedTaxa, o.reproductiveCondition, o.occurrenceRemarks, o.habitat, o.country, o.stateProvince, o.county, o.locality, o.locationRemarks, o.locationID, o.decimalLatitude, o.decimalLongitude, o.minimumElevationInMeters, o.permitURI, ms.materialSampleType, ms.storageLocation, ms.disposition, SUM(ms.numberCollected) AS totalNumberCollected, SUM(ms.numberAvailable) AS totalNumberAvailable, ms.sourcePlantCount, ms.preparationDate, ms.dateStored, ps.id AS preservedSpecimenTableID, ps.catalogNumber, ps.recordNumber FROM occurrences AS o LEFT JOIN projects as p ON o.projectTableID = p.id LEFT JOIN materialSamples AS ms ON o.id = ms.occurrenceTableID LEFT JOIN preservedSpecimens as ps on o.id = ps.occurrenceTableID WHERE o.id IS NOT NULL`
 //group by params
 var groupBy = ` GROUP BY ms.materialSampleType, o.scientificName, o.eventDate, o.locality`
@@ -19,86 +18,105 @@ var groupBy = ` GROUP BY ms.materialSampleType, o.scientificName, o.eventDate, o
 
 //function to define query and get materialSamples and associated metadata
 async function searchCollections(req, res) {
-    await new Promise(resolve => setTimeout(() => {
-        //project
-        if (req.body.project !== '') {
-            materialSampleCollectionsQuery.push(` AND p.project = '${req.body.project}'`);
-        }
-        //scientificName
-        if (req.body.scientificName !== '') {
-            materialSampleCollectionsQuery.push(` AND o.scientificName LIKE '%${req.body.scientificName}%'`);
-        }
-        //eventDate START DATE PROVIDED
-        if (req.body.eventEarlyDate !== '' && req.body.eventLateDate === '') {
-            materialSampleCollectionsQuery.push(` AND o.eventDate BETWEEN '${req.body.eventEarlyDate}' AND '2300-01-01'`)
-        }
-        //eventDate START DATE AND END DATE PROVIDED
-        if (req.body.eventEarlyDate !== '' && req.body.eventLateDate !== '') {
-            materialSampleCollectionsQuery.push(` AND o.eventDate BETWEEN '${req.body.eventEarlyDate}' AND '${req.body.eventLateDate}'`)
-        }
-        //eventDate END DATE PROVIDED
-        if (req.body.eventEarlyDate === '' && req.body.eventLateDate !== '') {
-            materialSampleCollectionsQuery.push(` AND o.eventDate BETWEEN '1900-01-01' AND '${req.body.eventLateDate}'`)
-        }
-        //stateProvince
-        if (req.body.stateProvince !== ''){
-            materialSampleCollectionsQuery.push(` AND o.stateProvince = '${req.body.stateProvince}'`)
-        }
-        //county
-        if (req.body.county !== ''){
-            materialSampleCollectionsQuery.push(` AND o.county = '${req.body.county}'`)
-        }
-        //locationID
-        if (req.body.locationID !== ''){
-            materialSampleCollectionsQuery.push(` AND o.locationID LIKE '%${req.body.locationID}%'`)
-        }
-        //locationRemarks
-        if (req.body.locationRemarks !== ''){
-            materialSampleCollectionsQuery.push(` AND o.locationRemarks LIKE '%${req.body.locationRemarks}%'`)
-        }
-        //recordedBy
-        if (req.body.recordedBy !== ''){
-            materialSampleCollectionsQuery.push(` AND o.recordedBy LIKE '%${req.body.recordedBy}%'`)
-        }
-        //recordNumber
-        if (req.body.recordNumber !== ''){
-            materialSampleCollectionsQuery.push(` AND ps.recordNumber LIKE '%${req.body.recordNumber}%'`)
-        }
-        //catalogNumber
-        if (req.body.catalogNumber !== '') {
-            materialSampleCollectionsQuery.push(` AND ps.catalogNumber = '${req.body.catalogNumber}'`);
-        }
-        //locality IS EXACTLY
-        if (req.body.locality !== '' && req.body.optradio === 'isExactly'){
-            materialSampleCollectionsQuery.push(` AND o.locality = '${req.body.locality}'`)
-        }
-        //locality STARTS WITH
-        if (req.body.locality !== '' && req.body.optradio === 'startsWith'){
-            materialSampleCollectionsQuery.push(` AND o.locality LIKE '${req.body.locality}%'`)
-        }
-        //locality CONTAINS
-        if (req.body.locality !== '' && req.body.optradio === 'contains'){
-            materialSampleCollectionsQuery.push(` AND o.locality LIKE '%${req.body.locality}%'`)
-        }
-        //materialSampleType
-        if (req.body.materialSampleType !== ''){
-            materialSampleCollectionsQuery.push(` AND ms.materialSampleType = '${req.body.materialSampleType}'`)
-        }
-        fullMaterialSampleCollectionsQuery = materialSampleCollectionsQuery.join()
-        queryParams = fullMaterialSampleCollectionsQuery.replaceAll(',','') //this is a problem when there is a comma in the locality data
-        collectionsQuery = materialSampleCollecitonsSelect.concat(queryParams)
-        finalCollectionsQuery = collectionsQuery.concat(groupBy)
-        materialSampleCollectionsQuery = []
-        console.log(finalCollectionsQuery)
-        resolve()
-    },1000))
-    sequelize.query(finalCollectionsQuery, { type:QueryTypes.SELECT })
+    let whereClauses = [];
+    let replacements = {};
+
+    //project
+    if (req.body.project !== '') {
+        whereClauses.push(`AND p.project = :project`);
+        replacements.project = req.body.project;
+    }
+    //scientificName
+    if (req.body.scientificName !== '') {
+        whereClauses.push(`AND o.scientificName LIKE :scientificName`);
+        replacements.scientificName = `%${req.body.scientificName}%`;
+    }
+    //eventDate START DATE PROVIDED
+    if (req.body.eventEarlyDate !== '' && req.body.eventLateDate === '') {
+        whereClauses.push(`AND o.eventDate BETWEEN :eventEarlyDate AND '2300-01-01'`);
+        replacements.eventEarlyDate = req.body.eventEarlyDate;
+    }
+    //eventDate START DATE AND END DATE PROVIDED
+    if (req.body.eventEarlyDate !== '' && req.body.eventLateDate !== '') {
+        whereClauses.push(`AND o.eventDate BETWEEN :eventEarlyDate AND :eventLateDate`);
+        replacements.eventEarlyDate = req.body.eventEarlyDate;
+        replacements.eventLateDate = req.body.eventLateDate;
+    }
+    //eventDate END DATE PROVIDED
+    if (req.body.eventEarlyDate === '' && req.body.eventLateDate !== '') {
+        whereClauses.push(`AND o.eventDate BETWEEN '1900-01-01' AND :eventLateDate`);
+        replacements.eventLateDate = req.body.eventLateDate;
+    }
+    //stateProvince
+    if (req.body.stateProvince !== '') {
+        whereClauses.push(`AND o.stateProvince = :stateProvince`);
+        replacements.stateProvince = req.body.stateProvince;
+    }
+    //county
+    if (req.body.county !== '') {
+        whereClauses.push(`AND o.county = :county`);
+        replacements.county = req.body.county;
+    }
+    //locationID
+    if (req.body.locationID !== '') {
+        whereClauses.push(`AND o.locationID LIKE :locationID`);
+        replacements.locationID = `%${req.body.locationID}%`;
+    }
+    //locationRemarks
+    if (req.body.locationRemarks !== '') {
+        whereClauses.push(`AND o.locationRemarks LIKE :locationRemarks`);
+        replacements.locationRemarks = `%${req.body.locationRemarks}%`;
+    }
+    //recordedBy
+    if (req.body.recordedBy !== '') {
+        whereClauses.push(`AND o.recordedBy LIKE :recordedBy`);
+        replacements.recordedBy = `%${req.body.recordedBy}%`;
+    }
+    //recordNumber
+    if (req.body.recordNumber !== '') {
+        whereClauses.push(`AND ps.recordNumber LIKE :recordNumber`);
+        replacements.recordNumber = `%${req.body.recordNumber}%`;
+    }
+    //catalogNumber
+    if (req.body.catalogNumber !== '') {
+        whereClauses.push(`AND ps.catalogNumber = :catalogNumber`);
+        replacements.catalogNumber = req.body.catalogNumber;
+    }
+    //locality IS EXACTLY
+    if (req.body.locality !== '' && req.body.optradio === 'isExactly') {
+        whereClauses.push(`AND o.locality = :locality`);
+        replacements.locality = req.body.locality;
+    }
+    //locality STARTS WITH
+    if (req.body.locality !== '' && req.body.optradio === 'startsWith') {
+        whereClauses.push(`AND o.locality LIKE :localityStartsWith`);
+        replacements.localityStartsWith = `${req.body.locality}%`;
+    }
+    //locality CONTAINS
+    if (req.body.locality !== '' && req.body.optradio === 'contains') {
+        whereClauses.push(`AND o.locality LIKE :localityContains`);
+        replacements.localityContains = `%${req.body.locality}%`;
+    }
+    //materialSampleType
+    if (req.body.materialSampleType !== '') {
+        whereClauses.push(`AND ms.materialSampleType = :materialSampleType`);
+        replacements.materialSampleType = req.body.materialSampleType;
+    }
+
+    const finalCollectionsQuery = materialSampleCollecitonsSelect
+        .concat(' ', whereClauses.join(' '))
+        .concat(groupBy);
+
+    console.log(finalCollectionsQuery, replacements)
+
+    sequelize.query(finalCollectionsQuery, { replacements, type: QueryTypes.SELECT })
     .then((data) => {
         res.send(data)
         dataForExport = data
     })
     .catch((err) => {
         console.log(err);
+        res.status(500).send({ message: "Query failed" });
       })
 }
 
