3ec2d4f0f1c8a98694fc1d35d60077cd223f57a2
Rick Levy <ricklevy21@gmail.com>
2026-09-02 09:06:14 -0600

    getSeedForTrial update

--- controllers/getSeedForTrial.js
+++ controllers/getSeedForTrial.js
@@ -7,47 +7,52 @@ const { sequelize } = require("../models");
 const { QueryTypes } = require('sequelize');
 const Op = Sequelize.Op;
 
-//empty array to populate with parameters that make up where clause
-var seedQuery =[]
-//standard query to return all seed records from materialSamples table. extra where params provided by user are concatted to the end of this string
+//standard query to return all seed records from materialSamples table. extra where params provided by user are added via named replacements
 var seedSelect = `SELECT ms.id, ms.materialSampleType, ms.materialSample_catalogNumber, ms.materialSample_recordNumber, ms.storageLocation, ms.disposition, ms.numberCollected, ms.numberAvailable, ms.sourcePlantCount, ms.preparationDate, ms.dateStored, o.scientificName, o.eventDate, o.recordedBy, o.county, o.stateProvince, o.county, o.locality, o.locationID, o.locationRemarks, o.decimalLatitude, o.decimalLongitude, o.minimumElevationInMeters, ps.catalogNumber FROM materialSamples AS ms LEFT JOIN occurrences AS o ON ms.occurrenceTableID = o.id LEFT JOIN preservedSpecimens AS ps ON o.id = ps.occurrenceTableID WHERE ms.materialSampleType = 'seed' `
 
 async function getSeedsForTrial(req, res) {
-    await new Promise(resolve => setTimeout(() => {
-        if (req.body.materialSample_catalogNumber !== '') {
-            seedQuery.push(` AND ms.materialSample_catalogNumber = '${req.body.materialSample_catalogNumber}'`);
-        }
+    let whereClauses = [];
+    let replacements = {};
 
-        if (req.body.scientificName !== '') {
-            seedQuery.push(` AND o.scientificName LIKE '%${req.body.scientificName}%'`);
-        }
+    if (req.body.materialSample_catalogNumber !== '') {
+        whereClauses.push(`AND ms.materialSample_catalogNumber = :catalogNumber`);
+        replacements.catalogNumber = req.body.materialSample_catalogNumber;
+    }
 
-        if (req.body.earlyDate !== '' && req.body.lateDate === '') {
-            seedQuery.push(` AND .eventDate BETWEEN '${req.body.earlyDate}' AND '2300-01-01'`)
-        }
+    if (req.body.scientificName !== '') {
+        whereClauses.push(`AND o.scientificName LIKE :scientificName`);
+        replacements.scientificName = `%${req.body.scientificName}%`;
+    }
 
-        if (req.body.earlyDate !== '' && req.body.lateDate !== '') {
-            seedQuery.push(` AND o.eventDate BETWEEN '${req.body.earlyDate}' AND '${req.body.lateDate}'`)
-        }
+    if (req.body.earlyDate !== '' && req.body.lateDate === '') {
+        whereClauses.push(`AND o.eventDate BETWEEN :earlyDate AND '2300-01-01'`);
+        replacements.earlyDate = req.body.earlyDate;
+    }
 
-        if (req.body.earlyDate === '' && req.body.lateDate !== '') {
-            seedQuery.push(` AND o.eventDate BETWEEN '1900-01-01' AND '${req.body.lateDate}'`)
-        }
-        fullSeedQuery = seedQuery.join()
-        //console.log(fullSeedQuery)
-        queryParams = fullSeedQuery.replaceAll(',','')
-        finalQuery = seedSelect.concat(queryParams)
-        finalQuery = finalQuery + "ORDER BY o.scientificName, o.eventDate"
-        seedQuery = []
-        resolve()
-    },1000))
-    console.log("FINAL QUERY" + finalQuery)
-    sequelize.query(finalQuery, { type:QueryTypes.SELECT })
+    if (req.body.earlyDate !== '' && req.body.lateDate !== '') {
+        whereClauses.push(`AND o.eventDate BETWEEN :earlyDate AND :lateDate`);
+        replacements.earlyDate = req.body.earlyDate;
+        replacements.lateDate = req.body.lateDate;
+    }
+
+    if (req.body.earlyDate === '' && req.body.lateDate !== '') {
+        whereClauses.push(`AND o.eventDate BETWEEN '1900-01-01' AND :lateDate`);
+        replacements.lateDate = req.body.lateDate;
+    }
+
+    const finalQuery = seedSelect
+        .concat(whereClauses.join(' '))
+        .concat(' ORDER BY o.scientificName, o.eventDate');
+
+    console.log("FINAL QUERY " + finalQuery, replacements)
+
+    sequelize.query(finalQuery, { replacements, type: QueryTypes.SELECT })
     .then((data) => {
         res.send(data)
     })
     .catch((err) => {
         console.log(err);
+        res.status(500).send({ message: "Query failed" });
       })
 }
 
